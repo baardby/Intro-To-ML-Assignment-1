03c0709a7fa20bbb2fa16efe453e1b4e50a3736b
Bård Bysveen <baardbysveen@proton.me>
2025-09-07 20:04:15 +0200

    Finalized logistic regregression in mission 2

--- Assignment1/logisitc_regression.py
+++ Assignment1/logisitc_regression.py
@@ -8,12 +8,12 @@ def sigmoid_lg(theta, X):
 def compute_gradient(theta, X, y):
     y_pred = sigmoid_lg(theta, X)
 
-    return 1/y.shape[0] * ( np.matmul(np.transpose(X), y_pred - y))
+    return 1/y.shape[0] * ( np.matmul(np.transpose(X), y_pred - y) )
 
 class LogisticRegression():
 
     def __init__(self):
-        self.learning_rate = 0.01
+        self.learning_rate = 0.1
         self.epochs = 1000
         self.weights = None
         self.losses, self.train_accuracies = [], []
@@ -23,18 +23,28 @@ class LogisticRegression():
 
     def fit_lg(self, trainData, alteredData):
         #Forandrer formatet på dataen til numpy
-        X = trainData[['x0', 'x1']].to_numpy()
         y = trainData[['y']].to_numpy()
 
-        #X kommer uten biaskolonne så legger til det
-        if alteredData:
+        #if setning for å prøve forskjellige måter å justere dataen på
+        if alteredData == 0: #Uten justeringer
+            #X kommer uten biaskolonne så legger til det
+            X = trainData[['x0', 'x1']].to_numpy()
+            bias = np.ones((X.shape[0], 1))
+            X = np.hstack((bias, X))
+        elif alteredData == 1: #Kun en feature: x0*x1
+            x0 = trainData['x0'].to_numpy()
+            x1 = trainData['x1'].to_numpy()
+            X = x0*x1
+            X = X.reshape((y.shape[0], 1))
+        elif alteredData == 2: #Tre features: x0, x1 og x0*x1
+            X = trainData[['x0', 'x1']].to_numpy()
             dummy = X[:, 0] * X[:, 1]
             dummy = dummy.reshape((y.shape[0], 1))
             X = np.hstack((X, dummy))
-        bias = np.ones((X.shape[0], 1))
-        X = np.hstack((bias, X))
+            bias = np.ones((X.shape[0], 1))
+            X = np.hstack((bias, X))
 
-        self.weights = np.zeros((X.shape[1], 1))
+        self.weights = np.ones((X.shape[1], 1))
 
         theta = self.weights
 
@@ -44,13 +54,23 @@ class LogisticRegression():
 
 
     def predict_lg(self, testData, alteredData):
-        X = testData[['x0', 'x1']].to_numpy()
-        if alteredData:
+        
+        if alteredData == 0: #Ujustert data
+            X = testData[['x0', 'x1']].to_numpy()
+            bias = np.ones((X.shape[0], 1))
+            X = np.hstack((bias, X))
+        elif alteredData == 1: #Kun en feature: x0*x1
+            x0 = testData['x0'].to_numpy()
+            x1 = testData['x1'].to_numpy()
+            X = x0*x1
+            X = X.reshape((x0.shape[0], 1))
+        elif alteredData == 2: #Tre features: x0, x1 og x0*x1
+            X = testData[['x0', 'x1']].to_numpy()
             dummy = X[:, 0] * X[:, 1]
             dummy = dummy.reshape((X.shape[0], 1))
             X = np.hstack((X, dummy))
-        bias = np.ones((X.shape[0], 1))
-        X = np.hstack((bias, X))
+            bias = np.ones((X.shape[0], 1))
+            X = np.hstack((bias, X))
 
-        y_pred = sigmoid_lg(self.weights, X) #GIR VELDIG SMÅ VERDIER. NESTEN SÅ DEN IKKE PRØVER
+        y_pred = sigmoid_lg(self.weights, X)
         return [1 if _y > 0.5 else 0 for _y in y_pred.flatten()]
